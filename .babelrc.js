2412b7bad35f603f580fa30d5ac4fff7687fd6da
Mikhail Donskoy <m.donsk@yandex.ru>
2020-02-26 17:10:38 +0300

    Setup build for Node.js env

--- .babelrc.js
+++ .babelrc.js
@@ -1,8 +1,19 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', {
-      modules: false,
-      loose: true
-    }],
-  ],
+  env: {
+    esm: {
+      presets: [
+        ['@babel/preset-env', {
+          modules: false,
+          loose: true,
+        }],
+      ],
+    },
+    node: {
+      presets: [
+        ['@babel/preset-env', {
+          loose: true,
+        }],
+      ],
+    },
+  },
 };
