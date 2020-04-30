febce33bdf30232fa49c773e7136fa79745e7f8e
Mikhail Donskoy <m.donsk@yandex.ru>
2020-04-30 16:48:27 +0300

    Rewrite omit to TS

--- lib/omit.ts
+++ lib/omit.ts
@@ -1,2 +1,2 @@
-export default (obj, ...keysToOmit) => Object.keys(obj)
+export default (obj: any, ...keysToOmit: string[]) => Object.keys(obj)
   .reduce((acc, key) => ({ ...acc, ...(keysToOmit.indexOf(key) >= 0 ? {} : { [key]: obj[key] }) }), {});
