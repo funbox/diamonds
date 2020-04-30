a5bbce8464af69295afe9b1302507d903d3f6661
Mikhail Donskoy <m.donsk@yandex.ru>
2020-04-30 16:50:47 +0300

    Rewrite query-string-to-object to TS

--- lib/query-string-to-object.ts
+++ lib/query-string-to-object.ts
@@ -1,7 +1,7 @@
-export default query => (query
+export default (query: string) => (query
   ? (/^[?]/.test(query) ? query.slice(1) : query)
     .split('&')
-    .reduce((params, param) => {
+    .reduce((params: Record<string, string> , param) => {
       const [key, value] = param.split('=');
       params[decodeURIComponent(key)] = value ? decodeURIComponent(value.replace(/\+/g, ' ')) : '';
       return params;
