f92ddee0052d3628bc0ca4993da8a7f444a5ed7c
Mikhail Donskoy <m.donsk@yandex.ru>
2020-04-30 17:47:41 +0300

    Setup ESLint to work with TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,33 @@
 module.exports = {
-  extends: '@funboxteam',
   env: {
     browser: true,
     node: true,
+  },
+  extends: [
+    '@funboxteam',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: '.',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'error',
+    '@typescript-eslint/no-use-before-define': 'off',
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions:['.js', '.jsx', '.ts', '.tsx'],
+      }
+    }
   },
 };
