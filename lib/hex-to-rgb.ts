0545943a87e8f6b833799d6dc303ed549eb72f06
Mikhail Donskoy <m.donsk@yandex.ru>
2020-04-30 16:31:14 +0300

    Rewrite hex-to-rgb to TS

--- lib/hex-to-rgb.ts
+++ lib/hex-to-rgb.ts
@@ -1,4 +1,10 @@
-export default (hex) => {
+type RGB = {
+  r: number,
+  g: number,
+  b: number,
+};
+
+export default (hex: string): RGB | null => {
   const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
   const longHex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(longHex);
