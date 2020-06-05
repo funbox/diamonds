555bf723ace3708a3ccff7fb3a92c61da8f6d3cc
Mikhail Donskoy <m.donsk@yandex.ru>
2020-06-05 16:27:48 +0300

    Fix omit returning value type

--- lib/omit.ts
+++ lib/omit.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export default (obj: Record<string, any>, ...keysToOmit: string[]): any => Object.keys(obj)
-  .reduce((acc, key) => {
+export default (obj: Record<string, any>, ...keysToOmit: string[]): Record<string, any> => Object.keys(obj)
+  .reduce<Record<string, any>>((acc, key) => {
     if (keysToOmit.indexOf(key) >= 0) return acc;
 
     return {
