696ab7f73f5a548cc530218a4e48854b3963fab9
Mikhail Donskoy <m.donsk@yandex.ru>
2020-06-19 18:16:21 +0300

    Simplify omit

--- lib/omit.ts
+++ lib/omit.ts
@@ -3,10 +3,9 @@
 
 export default (obj: Record<string, any>, ...keysToOmit: string[]): Record<string, any> => Object.keys(obj)
   .reduce<Record<string, any>>((acc, key) => {
-    if (keysToOmit.indexOf(key) >= 0) return acc;
+    if (keysToOmit.indexOf(key) === -1) {
+      acc[key] = obj[key];
+    }
 
-    return {
-      ...acc,
-      [key]: obj[key],
-    };
+    return acc;
   }, {});
