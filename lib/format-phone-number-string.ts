14c46fc7e47bf7e57c216f1001d39b0eae44e66f
Mikhail Donskoy <m.donsk@yandex.ru>
2021-02-12 12:23:36 +0300

    Replace plain space with non-breaking space in formatPhoneNumberString

--- lib/format-phone-number-string.ts
+++ lib/format-phone-number-string.ts
@@ -1,3 +1,3 @@
 export default (phone: number, { prefix = '+' } = {}): string => phone
   .toString()
-  .replace(/(\d)(\d{3})(\d{3})(\d{2})(\d{2})/g, `${prefix}$1 $2 $3-$4-$5`);
+  .replace(/(\d)(\d{3})(\d{3})(\d{2})(\d{2})/g, `${prefix}$1\xa0$2\xa0$3-$4-$5`);
