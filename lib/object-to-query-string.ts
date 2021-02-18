69a8f6a3ae8bbaacb0729df028264edfcf1470ee
Mikhail Donskoy <m.donsk@yandex.ru>
2021-02-18 16:13:52 +0300

    Support arrays in objectToQueryString

--- lib/object-to-query-string.ts
+++ lib/object-to-query-string.ts
@@ -1,7 +1,15 @@
-type QueryStringValue = string | number | boolean;
+type QueryStringValue = string | number | boolean | (string | number | boolean)[];
 
 const join = (obj: Record<string, QueryStringValue>): string => Object.keys(obj)
-  .map(key => `${key}=${encodeURIComponent(obj[key])}`)
+  .reduce((acc: string[], key) => {
+    const value = obj[key];
+
+    if (Array.isArray(value)) {
+      return acc.concat(value.map((item) => `${key}=${encodeURIComponent(item)}`));
+    }
+
+    return acc.concat(`${key}=${encodeURIComponent(value)}`);
+  }, [])
   .join('&');
 
 export default (params: Record<string, QueryStringValue>): string => `?${join(params)}`;
