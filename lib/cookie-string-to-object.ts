7b6223d45946300f5b6cffaeb50763bcea13abd7
igoradamenko <mail@igoradamenko.com>
2022-09-05 00:22:16 +0300

    Handle empty string case in cookieStringToObject

--- lib/cookie-string-to-object.ts
+++ lib/cookie-string-to-object.ts
@@ -1,7 +1,13 @@
-export default (cookiesStr: string): Record<string, string> => cookiesStr
-  .split(';')
-  .map(v => v.split('='))
-  .reduce((acc: Record<string, string>, v) => {
-    acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
-    return acc;
-  }, {});
+export default (cookiesStr: string): Record<string, string> => {
+  const cookiesStrTrimmed = cookiesStr.trim();
+
+  if (cookiesStrTrimmed.length === 0) return {};
+
+  return cookiesStrTrimmed
+    .split(';')
+    .map(v => v.split('='))
+    .reduce((acc: Record<string, string>, v) => {
+      acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+      return acc;
+    }, {});
+};
