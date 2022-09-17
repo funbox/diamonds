951c458e11f0d2e1360387425241a53a69124974
igoradamenko <mail@igoradamenko.com>
2022-09-17 19:50:09 +0300

    Make enableBodyScroll jump instead of smooth scrolling

--- lib/body-scroll.ts
+++ lib/body-scroll.ts
@@ -6,8 +6,21 @@ export function enableBodyScroll(): void {
 
     if (document.body.style.marginTop) {
       const scrollTop = -parseInt(document.body.style.marginTop, 10);
+
       document.body.style.marginTop = '';
-      window.scrollTo(window.pageXOffset, scrollTop);
+
+      window.scrollTo({
+        left: window.pageXOffset,
+        top: scrollTop,
+
+        // 'instant' is supported by Chrome and Firefox even so it was removed from the spec years ago
+        // also it still may be back: https://github.com/w3c/csswg-drafts/issues/3497
+        // it's useful here when the <html> has scroll-behavior: smooth set
+        // we don't want smooth scroll, we want the actual jump
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore
+        behavior: 'instant',
+      });
     }
   } else {
     window.addEventListener('load', enableBodyScroll);
