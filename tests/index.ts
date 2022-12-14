3aaaf2500c8be468bdf28624d6824bc17a794263
igoradamenko <mail@igoradamenko.com>
2022-12-14 14:20:11 +0200

    Add tests for formatNumberString

--- tests/index.ts
+++ tests/index.ts
@@ -1,9 +1,39 @@
 import { describe, it } from 'mocha';
 
+import { formatNumberString } from '../lib';
+
 const expect = require('chai').expect;
 
-describe('group', () => {
-  it('test', () => {
-    expect(1).to.be.eql(1);
+describe('formatNumberString', () => {
+  it('should return non formatted integer when its < 1000', () => {
+    expect(formatNumberString(100)).to.be.eql('100');
+  });
+
+  it('should return non formatted integer when its < 10000', () => {
+    expect(formatNumberString(1000)).to.be.eql('1000');
+  });
+
+  it('should return formatted integer when its < 10000 and formatThousands option passed', () => {
+    expect(formatNumberString(1000, { formatThousands: true })).to.be.eql('1 000');
+  });
+
+  it('should take account of custom formatting options', () => {
+    expect(
+      formatNumberString(-123456.78, {
+        space: '!',
+        delimiter: '@',
+        minus: '#',
+      }),
+    ).to.be.eql('#123!456@78');
+  });
+
+  it('should round floats to second digit after the delimiter', () => {
+    expect(formatNumberString(123.456)).to.be.eql('123,46');
+  });
+
+  it('should remove trailing zeros when formatting float', () => {
+    expect(formatNumberString(123.000)).to.be.eql('123');
+    expect(formatNumberString(123.100)).to.be.eql('123,1');
+    expect(formatNumberString(123.120)).to.be.eql('123,12');
   });
 });
