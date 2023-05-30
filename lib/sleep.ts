2143e913ceaf47b151539a55ba75e633ccabae2f
igoradamenko <mail@igoradamenko.com>
2023-05-30 14:59:32 +0300

    Do not return anything inside setTimeout in sleep fn

--- lib/sleep.ts
+++ lib/sleep.ts
@@ -1,3 +1,6 @@
+// TODO: deprecate in favor of timers/promise.setTimeout?
 export default function sleep(ms: number): Promise<void> {
-  return new Promise(r => setTimeout(r, ms));
+  return new Promise(r => {
+    setTimeout(r, ms);
+  });
 }
