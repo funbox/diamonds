9bf68404f4579e68a9a9d8973a4e97dc18e9824e
igoradamenko <mail@igoradamenko.com>
2023-05-30 14:40:56 +0300

    Rewrite deep-clone types to fix linting errors

--- lib/deep-clone.ts
+++ lib/deep-clone.ts
@@ -1,10 +1,17 @@
 // This function is too multipurpose so we allow `any` here.
 // Also there are some constructions that are allowed by linter, but the code will be much more complex w/o them.
-/* eslint-disable no-restricted-syntax, no-prototype-builtins, @typescript-eslint/no-explicit-any */
 
-function deepClone<T>(obj: T): T;
-function deepClone(obj: any): any {
-  if (obj === null || typeof obj !== 'object') return obj;
+type DeepCloneSupportedType = boolean | number | bigint | string | undefined | null | Date;
+type DeepCloneSupportedComplexTypes = DeepCloneSupportedType | Record<string, DeepCloneSupportedType> | Array<DeepCloneSupportedType>;
+
+// TODO: deprecate in favor of https://developer.mozilla.org/en-US/docs/Web/API/structuredClone
+function deepClone<T extends DeepCloneSupportedType>(obj: T): T;
+function deepClone<T extends Record<string, DeepCloneSupportedType>>(obj: T): T;
+function deepClone<T extends Array<DeepCloneSupportedType>>(obj: T): T;
+function deepClone(obj: DeepCloneSupportedComplexTypes): DeepCloneSupportedComplexTypes {
+  if (obj == null || typeof obj !== 'object') {
+    return obj;
+  }
 
   if (obj instanceof Date) {
     const copy = new Date();
@@ -13,24 +20,20 @@ function deepClone(obj: any): any {
   }
 
   if (obj instanceof Array) {
-    const copy = [];
+    const copy: Array<DeepCloneSupportedType> = [];
     for (let i = 0, len = obj.length; i < len; i++) {
       copy[i] = deepClone(obj[i]);
     }
     return copy;
   }
 
-  if (obj instanceof Object) {
-    const copy: Record<any, any> = {};
-    for (const attr in obj) {
-      if (obj.hasOwnProperty(attr)) {
-        copy[attr] = deepClone(obj[attr]);
-      }
-    }
-    return copy;
-  }
+  const copy: typeof obj = {};
 
-  return obj;
+  Object.keys(obj).forEach(key => {
+    copy[key] = deepClone(obj[key]);
+  });
+
+  return copy;
 }
 
 export default deepClone;
