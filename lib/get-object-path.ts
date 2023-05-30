22fe30ebbf0331b6537b49411b3dbdd7f4f78204
igoradamenko <mail@igoradamenko.com>
2023-05-30 14:54:17 +0300

    Mute @typescript-eslint/no-unsafe-return in get-object-path

--- lib/get-object-path.ts
+++ lib/get-object-path.ts
@@ -1,5 +1,5 @@
 // The function may be used for getting value of any type, so we allow `any` uage here.
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return */
 /** @deprecated since v6.5.0 - use optional chaining instead. */
 
 export default (obj: Record<string, any>, path: string): any => path.toString()
