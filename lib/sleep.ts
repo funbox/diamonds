8ad8078d9021040661e57f319b0a2a6b194c54c4
igoradamenko <mail@igoradamenko.com>
2023-05-31 14:36:08 +0300

    Add @see link to sleep helper

--- lib/sleep.ts
+++ lib/sleep.ts
@@ -1,4 +1,6 @@
-// TODO: deprecate in favor of timers/promise.setTimeout?
+/**
+ * @see {@link https://nodejs.org/dist/latest-v18.x/docs/api/timers.html#timerspromisessettimeoutdelay-value-options|Node.js API doc} for setTimeout from timers/promise module which may replace this helper
+ */
 export default function sleep(ms: number): Promise<void> {
   return new Promise(r => {
     setTimeout(r, ms);
